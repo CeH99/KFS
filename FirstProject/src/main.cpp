628b63393ee8cc8031f09b9440e983fbe4e6c56d
CeH99 <senya183381@gmail.com>
2025-09-19 18:51:10 +0000

    19/09/2025

--- FirstProject/src/main.cpp
+++ FirstProject/src/main.cpp
@@ -1,90 +1,83 @@
 #include <Arduino.h>
 
-class Led
-{
-  private:
-      int pin_;
-      bool state_;
-  public:
-      Led(int pin) : pin_(pin), state_(false)
-      {
-          pinMode(pin_, OUTPUT);
-      }
+class Led {
+private:
+    int pin_;
+    bool state_;
 
-      void on()
-      {
+public:
+    Led(int pin) : pin_(pin), state_(false) {
+        pinMode(pin_, OUTPUT);
+    }
+
+    void on() {
+        digitalWrite(pin_, HIGH);
         state_ = true;
-        digitalWrite(pin_, 1);
-      }
+    }
 
-      void off()
-      {
+    void off() {
+        digitalWrite(pin_, LOW);
         state_ = false;
-        digitalWrite(pin_, 0);
-      }
+    }
 
-      void toggle()
-      {
-        if(state_)
-          off();
-        else
-          on();
-      }
+    void toggle() {
+        if (state_) off();
+        else on();
+    }
 
-      bool getState()
-      {
+    bool isOn() const {
         return state_;
-      }
+    }
 };
 
-class Button
-{
-  private:
-      int pin_;
-      bool lastState_;
-      unsigned long int lastPress_;
-  public:
-      Button(int pin) : pin_(pin), lastState_(HIGH), lastPress_(0)
-      {
-        pinMode(pin_, INPUT_PULLUP);
-      }
+class LedController {
+private:
+    Led* leds_;
+    int count_;
 
-  public:
-      bool isPressed()
-      {
-        return digitalRead(pin_) == LOW;
-        
-      }
+public:
+    LedController(Led* leds, int count) : leds_(leds), count_(count) {}
 
-      bool wasPressed()
-      {
-        bool current = digitalRead(pin_);
+    void trafficLight(int redTime, int yellowTime, int greenTime, int blinkTime) {
+        enum { RED = 0, YELLOW, GREEN };
 
-        if(lastState_ == HIGH && current == LOW && (millis() - lastPress_) > 50)
-        {
-          lastState_ = current;
-          lastPress_ = millis();
-          return true;        
+        // red
+        leds_[RED].on();
+        delay(redTime);
+        leds_[RED].off();
+        blink(RED, 5, blinkTime);
+
+        // yellow
+        leds_[YELLOW].on();
+        delay(yellowTime);
+        leds_[YELLOW].off();
+
+        //green
+        leds_[GREEN].on();
+        delay(greenTime);
+        leds_[GREEN].off();
+        blink(GREEN, 5, blinkTime);
+    }
+
+private:
+    void blink(int index, int times, int time) {
+        for (int i = 0; i < times; ++i) {
+            leds_[index].on();
+            delay(time);
+            leds_[index].off();
+            delay(time);
         }
-          lastState_ = current;
-          return false;
-      }
+    }
 };
 
-  Led led(8);
-  Button b1(A5);
+Led leds[] = { Led(13), Led(12), Led(11) };
+LedController controller(leds, 3);
 
-void setup()
-{
-  Serial.begin(9600);
+void setup() {
+    Serial.begin(9600);
 }
 
-void loop()
-{
-   if (b1.wasPressed())
-  {
-    led.toggle();
-    Serial.println("Toggled LED");
-  }
-  
+void loop() {
+    controller.trafficLight(6000, 2000, 6000, 400);
+    delay(200);
 }
